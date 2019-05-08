c80d757b80952dbfee2b9069baba1d564cb98aa1
GrandFisher <470911438@qq.com>
2019-05-08 21:40:30 +0800

    init

---
+++ src/main/java/cn/sjtu/springwebjpa/SpringwebjpaApplication.java
@@ -0,0 +1,13 @@
+package cn.sjtu.springwebjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebjpaApplication.class, args);
+	}
+
+}
